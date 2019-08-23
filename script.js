10fc63d4678c306626676fe91ae6de7ad8ead747
Alexander <a.strumilov@gmail.com>
2019-08-23 21:42:04 +0300

    updated

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const addTaskBtn = document.querySelector('.task-block__btn_add'),
-    data = [],
+const data = [],
+    addTaskBtn = document.querySelector('.task-block__btn_add'),
     taskInput = document.querySelector('.task-block__input-value'),
     taskList = document.querySelector('.task-list'),
     taskWrap = document.querySelector('.task-block'),
@@ -9,7 +9,6 @@ const addTaskBtn = document.querySelector('.task-block__btn_add'),
         liAttr: 'data-index',
         span: 'span',
         spanClass: 'task-list__item-close',
-        spanContent: '\2718',
         create: function (value, i) {
             let li = document.createElement(this.li),
                 span = document.createElement(this.span);
@@ -17,11 +16,13 @@ const addTaskBtn = document.querySelector('.task-block__btn_add'),
             span.innerHTML = '&#10008;'
             li.classList.add(this.liClass);
             li.setAttribute(this.liAttr, i)
-            li.innerHTML = `Задача <span>«${value}»</span> создана.`,
-                li.appendChild(span);
-            taskList.appendChild(li);
+            li.innerHTML = `Задача: <span>«${value}»</span>.`,
+            li.appendChild(span);
+            return li;
         }
     }
+
+init();
 
 function init() {
     if (localStorage.getItem('tasks')) {
@@ -29,25 +30,29 @@ function init() {
         let i = 0;
         while (i < initData.length) {
             data.push(initData[i]);
-            taskElement.create(initData[i].value, i)
+            let element = taskElement.create(data[i].value, i);
+            taskList.appendChild(element);
+            if (!data[i].visible) {
+                element.classList.add('task-list__item_done');
+            }
             i++;
         }
     }
 }
-init();
-
+// Добавить задачу
 function addTask(val) {
     if (val !== "") {
-        taskElement.create(val, data.length)
+        let element = taskElement.create(val, data.length);
+        taskList.appendChild(element);
         data.push({
             'value': val,
             'visible': true
         });
         taskInput.value = ''
-        updateStorage(data);
+        updateStorage();
     }
 }
-
+// Удалить или вычеркнуть задачу
 function removeTask(e) {
     let tasks = document.getElementsByClassName('task-list__item'),
         tasksClose = document.getElementsByClassName('task-list__item-close'),
@@ -58,20 +63,22 @@ function removeTask(e) {
             taskList.removeChild(tasks[i])
         }
         if (tasks[i] == target && tasks[i].matches('li.task-list__item')) {
+            data[i].visible = !data[i].visible;
             if (tasks[i].classList.contains('task-list__item_done')) {
-                tasks[i].classList.remove('task-list__item_done')
+                tasks[i].classList.remove('task-list__item_done');
             } else {
-                tasks[i].classList.add('task-list__item_done')
+                tasks[i].classList.add('task-list__item_done');
             }
         }
     }
-    updateStorage(data);
+    updateStorage();
+}
+// Обновить хранилище
+function updateStorage() {
+    localStorage.setItem('tasks', JSON.stringify(data));
 }
 
-function updateStorage(arg) {
-    localStorage.setItem('tasks', JSON.stringify(arg));
-}
-
+// Обработчики
 addTaskBtn.addEventListener('click', () => {
     addTask(taskInput.value);
 });
